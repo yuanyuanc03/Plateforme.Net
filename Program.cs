c55ab9c271f0e557a4fb1f8eeab8bd2badbf94a5
yuanyuanc03 <56487296+yuanyuanc03@users.noreply.github.com>
2020-05-12 09:36:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bacchus.MVC.Controller;
+using Bacchus.MVC.DAO;
+using Bacchus.MVC.Model;
+using Bacchus.MVC.View.MainView;
+
+namespace Bacchus
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //ModelListDao.EmptyDataBase();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
